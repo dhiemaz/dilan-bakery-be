5940af43395e85f3081a49b8fd96548534d9c208
Dimas <prawira.dimas.yudha@gmail.com>
2024-06-26 22:42:40 +0700

    refactoring, add cloudinary and change multer to inmemory.

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,53 +1,64 @@
 const foodModel = require("../models/foodModel.js");
+const cloudinary = require("cloudinary").v2;
 const fs = require('fs')
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 //add food item
-const addFood = async (req,res)=>{
+const addFood = async (req, res) => {
 
-let image_filename = `${req.file.filename}`;
+    // Upload image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path , {
+        folder:'folder_name'
+     });
 
-const food = new foodModel({
-    name:req.body.name,
-    description:req.body.description,
-    price:req.body.price,
-    category:req.body.category,
-    image:image_filename
-})
+    let image_filename = result.secure_url;
 
-try {
-    await food.save();
-    res.json({success:true,message:"Food Added"})
-} catch (error) {
-    console.log(error)
-    res.json({success:false,message:"Error"})
-    
-}
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        image: image_filename
+    })
+
+    try {
+        await food.save();
+        res.json({ success: true, message: "Food Added" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+
+    }
 }
 
 //all food list
-const listFood = async (req,res) => {
+const listFood = async (req, res) => {
     try {
         const foods = await foodModel.find({});
-        res.json({success:true,data:foods})
+        res.json({ success: true, data: foods })
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
-        
+        res.json({ success: false, message: "Error" })
     }
 }
 
 //remove food item
-const removeFood = async (req,res) =>{
+const removeFood = async (req, res) => {
     try {
         const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`,()=>{})
+        fs.unlink(`uploads/${food.image}`, () => { })
 
         await foodModel.findByIdAndDelete(req.body.id);
-        res.json({success:true,message:"Food Removed"})
+        res.json({ success: true, message: "Food Removed" })
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
+        res.json({ success: false, message: "Error" })
     }
 }
 
-module.exports = {addFood,listFood,removeFood}
+module.exports = { addFood, listFood, removeFood }
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,21 +1,22 @@
-const { addFood,listFood,removeFood } = require("../controllers/foodController.js")
-const express = require("express")
-const multer = require("multer")
+const { addFood, listFood, removeFood } = require("../controllers/foodController.js");
+const express = require("express");
+const upload = require("../config/upload.js");
 
 const foodRouter = express.Router();
 
 //image storage engine
-const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination:"uploads",
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${Date.now()}${file.originalname}`)
+//     }
+// })
 
-const upload = multer({storage:storage})
+// const upload = multer({storage:storage})
 
-foodRouter.post("/add",upload.single("image"),addFood)
-foodRouter.get("/list",listFood)
-foodRouter.post("/remove",removeFood);
+foodRouter.post("/add", upload.single("image"), addFood);
+
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 module.exports = foodRouter;
