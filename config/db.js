b8a8a6c815e3b57400b5f202e359a3d1a20562a4
Dimas <prawira.dimas.yudha@gmail.com>
2024-06-27 01:05:11 +0700

    update mongo uri.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
 const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://audidiandra31:krPlsb6OVlI2tA9g@cluster0.bl3mhew.mongodb.net/dilan-bakery').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://dimas:7WJuu9bwfIsnGMrn@cluster0.lxswgdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
+    // await mongoose.connect('mongodb+srv://audidiandra31:krPlsb6OVlI2tA9g@cluster0.bl3mhew.mongodb.net/dilan-bakery').then(()=>console.log("DB Connected"));
 
 }
 
